d7a3f85ea4b980fc35bcc47d043052c5885eefdc
tjlyttles <tjlyttles84@gmail.com>
2019-07-12 19:42:57 -0700

    everything works!

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+const styles = {
+  navStyle: {
+    background: "#37444b",
+    height: "100px"
+  }
+};
+
+const Navbar = ({ score, highestScore, toggleTheme, theme }) => {
+  return (
+    <nav style={styles.navStyle}>
+      <div class="nav-wrapper">
+        
+        <p className="scores">Your Score: {score} </p>
+        <p className="space-around highScores">High Score: {highestScore} </p>
+        
+       <h1><a href="/" class="brand-logo center">
+          Clicker Game
+        </a></h1> 
+        <button className="btn-clear" onClick={toggleTheme}>
+          {theme === "light" ? "🔦" : "💡"}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
